1c234566bd8152a058cd9039815511787b5a594d
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2026-04-07 23:43:35 +0500

    Added small Changes

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,7 @@ import { login, register } from "../controllers/user.controller.js"
 
 const router = express.Router()
 
-router.post("/signup",register)
+router.post("/register",register)
 router.post("/login",login)
 
 export default router;
