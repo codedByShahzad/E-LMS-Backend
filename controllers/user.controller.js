16910700347ced53ea0509c3efca7cda17fc7d3b
MrShahzadDeveloper <mr.shahzad.developer@gmail.com>
2026-04-16 14:37:45 +0500

    Made the Log out APi

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -89,15 +89,22 @@ const login = async (req, res) => {
 
 const logout = async (req, res) => {
   try {
-    res.status(200).cookie("token", "", { maxAge: 0 }).json({
+    res.cookie("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    return res.status(200).json({
       success: true,
-      message: "Logged out Successfully",
+      message: "Logged out successfully",
     });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
       success: false,
-      message: "Failed to Logout", // ✅ fixed
+      message: "Failed to logout",
     });
   }
 };
